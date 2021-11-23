8196e4099d233ade03df7972862a8d7915fdda4f
Matin Kaboli <matinkaboli@aol.com>
2021-11-23 18:55:23 +0330

    fix: add validation to register page

--- models/index.go
+++ models/index.go
@@ -6,7 +6,7 @@ import (
 
 type User struct {
 	gorm.Model
-	Name     string `gorm:"name"`
-	Phone    string `gorm:"phone"`
-	Password string `gorm:"password"`
+	Name     string `gorm:"column:name; not null" json:"name"`
+	Phone    string `gorm:"column:phone;not null;unique" json:"phone"`
+	Password string `gorm:"column:password;not null" json:"password"`
 }
--- routes/v1/users/register.go
+++ routes/v1/users/register.go
@@ -1,6 +1,9 @@
 package users
 
 import (
+	"crypto/sha256"
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/matinkaboli/goods-golang/database"
 	"github.com/matinkaboli/goods-golang/models"
@@ -15,15 +18,20 @@ func RegisterUsersHandler(c *fiber.Ctx) error {
 		return c.SendStatus(fiber.StatusBadRequest)
 	}
 
+	hashed := sha256.Sum256([]byte(password))
+	hashed2 := fmt.Sprintf("%x", hashed)
+
 	db := database.DB
 
-	db.Create(&models.User{
+	user := &models.User{
 		Name:     name,
 		Phone:    phone,
-		Password: password,
-	})
+		Password: hashed2,
+	}
 
-	return c.JSON(fiber.Map{
-		"ok": true,
-	})
+	if dbc := db.Create(user); dbc.Error != nil {
+		return c.SendStatus(fiber.StatusConflict)
+	}
+
+	return c.JSON(&user)
 }
